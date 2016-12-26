438f3d8f804c7370aaac67a7d23e7bd82ef0bd9b
FreeDevAlexNesterov <FreeDev.AlexNesterov@mail.ru>
2016-12-26 10:50:31 +0300

    new

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Studio Design Web</title>
+</head>
+
+<body>
+    <!-- header -->
+<div class="container">
+
+	<header id="navtop">
+		<a href="index.html" class="logo fleft">
+			<img src="img/logo.png" alt="Designa Studio">
+		</a>
+		
+		<nav class="fright">
+			<ul>
+				<li><a href="index.html" class="navactive">Home</a></li>
+				<li><a href="about.html">About</a></li>
+			</ul>
+			<ul>
+				<li><a href="works.html">Works</a></li>
+				<li><a href="services.html">Services</a></li>
+			</ul>
+			<ul>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+    <!-- <img src="images/promo.jpg"> -->
+    <!-- <div class="karkas"></div>
+		<div class="header"></div>
+			<a href="#"><img class="logo" src="images/logo.jpg" alt="Company FreeNJ" ></a>
+			<ul>
+				<li><a href="#">main</a></li>
+				<li><a href="#">service</a></li>
+				<li><a href="#">price</a></li>
+				<li><a href="#">about</a></li>
+				<li><a href="#">contacts</a></li>
+			</ul>
+
+		<div class="main-content">
+			<div class="main-image-content">
+				<img src="images/content-img.jpg" alt="">
+
+			</div>
+
+<div class="content-left">
+
+			</div>
+
+
+			<div class="right-bar">
+
+			</div>
+
+			<div class="footer"></div>
+
+	</div>
+ -->
+ 
+ <?php
+    echo "222";
+ ?>
+</body>
+
+</html>
